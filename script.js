ebfe8dfc096dcdd44db21cb725386951e0faf107
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2023-10-19 20:19:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+function playAudio(filename) {
+    var audio = document.getElementById("audioPlayer");
+    audio.src = "sounds/" + filename;
+    audio.play();
+}
+
+function stopAudio() {
+    var audio = document.getElementById("audioPlayer");
+    audio.pause();
+    audio.currentTime = 0;
+}
+
